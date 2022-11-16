a673e78f8ee1b4bb4d8c968d83f393ff14ff8633
Heliton Lima <helitonmartinsdelima@gmail.com>
2022-11-16 12:14:39 -0300

    Tela dupla #57

--- src/app/views/duo/duo-page/duo-page.component.ts
+++ src/app/views/duo/duo-page/duo-page.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DuoPageComponent implements OnInit {
 
+  public messages = [
+    {
+      nickname: 'Pixer',
+      lastMessage: 'Última menssagem recebida...',
+      unreadMessage: 2
+    },
+    {
+      nickname: 'Juzt',
+      lastMessage: 'Última menssagem recebida...',
+      unreadMessage: 4
+    },
+    {
+      nickname: 'Bubble',
+      lastMessage: 'Última menssagem recebida...',
+      unreadMessage: 0
+    },
+    {
+      nickname: 'Bloodreina',
+      lastMessage: 'Última menssagem recebida...',
+      unreadMessage: 2
+    },
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/views/duo/duo.module.ts
+++ src/app/views/duo/duo.module.ts
@@ -1,17 +1,23 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'src/assets/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DuoRoutingModule } from './duo-routing.module';
 import { DuoPageComponent } from './duo-page/duo-page.component';
+import { MessageComponent } from './message/message.component';
 
 
 @NgModule({
   declarations: [
-    DuoPageComponent
+    DuoPageComponent,
+    MessageComponent
   ],
   imports: [
     CommonModule,
-    DuoRoutingModule
+    DuoRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class DuoModule { }
