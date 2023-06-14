45c4945ca10d4b8baa1b768cfc2949d7d0a9ca09
Heliton <helitonmartinsdelima@gmail.com>
2023-06-14 00:04:35 -0300

    Ajuste auth

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,10 +2,15 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: AuthComponent,
+  },
+  {
+    path: 'login',
     component: LoginComponent,
   },
   {
